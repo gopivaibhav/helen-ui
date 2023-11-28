cdd78e34322804d87d0db3b63e01dea09f95d746
Gopi Vaibhav <gopivaibhav0@gmail.com>
2023-11-28 15:00:41 +0530

    Reduced gap to 1sec

--- src/components/Helen.js
+++ src/components/Helen.js
@@ -31,7 +31,7 @@ const Helen = ({ topic = "" }) => {
       }
       SpeechRecognition.startListening({ language: "en-UK", continuos: true });
       console.log("listening>>>>>>>>>> ", listening);
-    }, 3000);
+    }, 1000);
   };
 
   // useEffect(() => {
