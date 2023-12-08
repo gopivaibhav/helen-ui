d2f185a2cec6252cb0bf8c88cfa90f3030f035d9
Mitsuha Miyamizu <rishabh@collegeit.org>
2023-12-08 16:03:12 +0530

    transcript added

--- src/components/Helen.js
+++ src/components/Helen.js
@@ -192,7 +192,7 @@ const Helen = ({ topic = "" }) => {
           </span>
         )}
       </div>
-      {/* <div
+      <div
         style={{
           width: "100%",
           height: "40vh",
@@ -206,7 +206,7 @@ const Helen = ({ topic = "" }) => {
         }}
       >
         {transcript}
-      </div> */}
+      </div>
       <div style={{ position: "absolute", right: 10, bottom: "20vh" }}>
         <div className={userRippleEffect ? "ripple-effect-user" : ""} />
         <img
