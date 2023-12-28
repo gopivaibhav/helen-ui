de329bea1dc751ed0419db861d0bb621eb2bf092
rishabh gaud <rishabh.kumbh@gmail.com>
2023-12-28 11:38:24 +0530

    seat work done

--- src/pages/HelenMain.js
+++ src/pages/HelenMain.js
@@ -5,6 +5,7 @@ import PreviousSession from "../components/PreviousSession";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 import { RingLoader } from "react-spinners";
+import "../styles/NewSession.css";
 const fetchUser = async (email) => {
   try {
     const user = await axios.get(
@@ -60,8 +61,33 @@ const HelenMain = () => {
     <div>
       <UserPofile />
       <NewSession userId={userData && userData._id ? userData._id : ""} />
-      {filledUserCount > 100 ? (
-        <div>total number of users: {filledUserCount}</div>
+      {filledUserCount <= 100 && !isAuthenticated ? (
+        <div style={{ marginLeft: "9.3vw", marginTop: "15px" }}>
+          <div
+            className="LeftCard"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              textAlign:"center",
+              alignItems: "center",
+              background: "#FF7777",
+              color: "white",
+              fontSize: 24,
+              fontFamily: "Nunito Sans",
+              fontWeight: "800",
+              lineHeight: "40px",
+              wordWrap: "break-word",
+              width: "91%",
+              height: "275px",
+              marginBottom: "20px",
+              marginLeft: "1px",
+              margin: "30px 8vw 30px 0px",
+            }}
+          >
+            Unlock tranquility! Free therapy for the first 100. Hurry, only{" "}
+            {100 - filledUserCount} spots left! Register now!
+          </div>
+        </div>
       ) : (
         <PreviousSession
           sessionDetail={userData && userData.sessions ? userData.sessions : ""}
