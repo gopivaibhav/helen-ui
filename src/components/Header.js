7437e95b352539e44e4000e0ac252b6f05de941e
Gopi Vaibhav <gopivaibhav0@gmail.com>
2024-01-10 20:24:16 +0530

    Tooltip for exit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import RefreshIcon from "@mui/icons-material/Refresh";
+import { Tooltip } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import CustomConfirmModal from "./CustomConfirmModal";
 import ProgressBar from "./ProgressBar";
@@ -43,41 +43,35 @@ const Header = ({ setIsActive, progress }) => {
   };
   return (
     <>
-      <button
-        style={{
-          position: "absolute",
-          top: "0",
-          left: "5px",
-          margin: "1rem",
-          cursor: "pointer",
-          outline: "none",
-          border: "none",
-          // border: "2px solid rgba(117, 139, 255, 1)", // Added border property with a solid red color
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "transparent",
-          color: "rgba(117, 139, 255, 1)",
-          padding: "0.2rem 10px",
-          borderRadius: "10px",
-          marginRight: "0.5rem",
-          marginLeft: "0.5rem",
-          fontSize: "16px",
-          fontWeight: "700 !important",
-          fontFamily: "'Nunito Sans', sans-serif ",
-        }}
-        onClick={handleBackButton}
-      >
-        {/* <img
+      <Tooltip title="Exit Session" placement="bottom-end">
+        <button
           style={{
-            width: "20px",
-            height: "20px",
+            position: "absolute",
+            top: "0",
+            left: "5px",
+            margin: "1rem",
+            cursor: "pointer",
+            outline: "none",
+            border: "none",
+            // border: "2px solid rgba(117, 139, 255, 1)", // Added border property with a solid red color
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "transparent",
+            color: "rgba(117, 139, 255, 1)",
+            padding: "0.2rem 10px",
+            borderRadius: "10px",
+            marginRight: "0.5rem",
+            marginLeft: "0.5rem",
+            fontSize: "16px",
+            fontWeight: "700 !important",
+            fontFamily: "'Nunito Sans', sans-serif ",
           }}
-          src="/back_button.svg"
-          alt="back button"
-        /> */}
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABEUlEQVR4nO2XPwrCMBSHP/QOvYJeQp11UvAqegAXBz2BTupN2jrp5qSjXcVNilOlkEKI6V+jRe0PMjzykveRvDzyoJI5TYEb4ABjoMmHdQcCZZyAGdAG6q8G6AGeJogr5iN7CKyAi+IX2mugXxTG0wQPh60ARAqDtMQJHJU1yyIAgRIg73wDGAkfvwyAvH5PqgCCv7kCC9iLiibLkZ7c2wAs4CAcdhk2MwpgScEPwi4KsJUqZGaAfUylSxpuzMZJp2EUwDYJYBm8AleTxKkApSehDLHTPLmkxPqtQhSnCiCoroCSv2ReSjnO8yldFAHoAmcNQFTxsnzLN8AAqPEG+TGNyRzomGhM0jQBrmW2ZnylHv7/6ByT+tNxAAAAAElFTkSuQmCC" />
-      </button>
+          onClick={handleBackButton}
+        >
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABEUlEQVR4nO2XPwrCMBSHP/QOvYJeQp11UvAqegAXBz2BTupN2jrp5qSjXcVNilOlkEKI6V+jRe0PMjzykveRvDzyoJI5TYEb4ABjoMmHdQcCZZyAGdAG6q8G6AGeJogr5iN7CKyAi+IX2mugXxTG0wQPh60ARAqDtMQJHJU1yyIAgRIg73wDGAkfvwyAvH5PqgCCv7kCC9iLiibLkZ7c2wAs4CAcdhk2MwpgScEPwi4KsJUqZGaAfUylSxpuzMZJp2EUwDYJYBm8AleTxKkApSehDLHTPLmkxPqtQhSnCiCoroCSv2ReSjnO8yldFAHoAmcNQFTxsnzLN8AAqPEG+TGNyRzomGhM0jQBrmW2ZnylHv7/6ByT+tNxAAAAAElFTkSuQmCC" />
+        </button>
+      </Tooltip>
       {showModal && (
         <CustomConfirmModal
           message="Do you really want to exit this session?"
